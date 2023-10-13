4ee6de9e3a871fa8f7a4afd0ea56c38789ec16fa
lukasvasadi <lukas.vasadi@gmail.com>
2023-10-13 18:10:39 -0400

    Add application signature configuration (commented out)

--- forge.config.ts
+++ forge.config.ts
@@ -1,26 +1,38 @@
-import type {ForgeConfig} from "@electron-forge/shared-types"
-import {MakerSquirrel} from "@electron-forge/maker-squirrel"
-import {MakerZIP} from "@electron-forge/maker-zip"
-import {MakerDeb} from "@electron-forge/maker-deb"
-import {MakerRpm} from "@electron-forge/maker-rpm"
-import {AutoUnpackNativesPlugin} from "@electron-forge/plugin-auto-unpack-natives"
-import {WebpackPlugin} from "@electron-forge/plugin-webpack"
-import * as fs from "fs"
-import * as path from "path"
-import spawn from "cross-spawn"
+import type { ForgeConfig } from '@electron-forge/shared-types'
+import { MakerSquirrel } from '@electron-forge/maker-squirrel'
+import { MakerZIP } from '@electron-forge/maker-zip'
+import { MakerDeb } from '@electron-forge/maker-deb'
+import { MakerRpm } from '@electron-forge/maker-rpm'
+import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives'
+import { WebpackPlugin } from '@electron-forge/plugin-webpack'
+import * as fs from 'fs'
+import * as path from 'path'
+import spawn from 'cross-spawn'
 
-import {mainConfig} from "./webpack.main.config"
-import {rendererConfig} from "./webpack.renderer.config"
+import { mainConfig } from './webpack.main.config'
+import { rendererConfig } from './webpack.renderer.config'
 
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
-        executableName: "vterm",
-        icon: "assets/icon", // no file extension required
-        extraResource: ["assets"],
+        executableName: 'vterm',
+        icon: 'assets/icon', // no file extension required
+        extraResource: ['assets']
+        // osxSign: {},
+        // osxNotarize: {
+        //     tool: 'notarytool',
+        //     appleId: process.env.APPLE_ID,
+        //     appleIdPassword: process.env.APPLE_PASSWORD,
+        //     teamId: process.env.APPLE_TEAM_ID
+        // }
     },
     rebuildConfig: {},
-    makers: [new MakerSquirrel({}), new MakerZIP({}, ["darwin"]), new MakerRpm({}), new MakerDeb({})],
+    makers: [
+        new MakerSquirrel({}),
+        new MakerZIP({}, ['darwin']),
+        new MakerRpm({}),
+        new MakerDeb({})
+    ],
     plugins: [
         new AutoUnpackNativesPlugin({}),
         new WebpackPlugin({
@@ -29,39 +41,39 @@ const config: ForgeConfig = {
                 config: rendererConfig,
                 entryPoints: [
                     {
-                        html: "./src/index.html",
-                        js: "./src/renderer.ts",
-                        name: "main_window",
+                        html: './src/index.html',
+                        js: './src/renderer.ts',
+                        name: 'main_window',
                         preload: {
-                            js: "./src/preload.ts",
-                        },
-                    },
-                ],
-            },
-        }),
+                            js: './src/preload.ts'
+                        }
+                    }
+                ]
+            }
+        })
     ],
     hooks: {
         packageAfterPrune: async (_, buildPath, __, platform) => {
             const commands = [
-                "install",
-                "--no-package-lock",
-                "--no-save",
-                "serialport",
+                'install',
+                '--no-package-lock',
+                '--no-save',
+                'serialport'
             ]
 
             return new Promise((resolve, reject) => {
-                const oldPckgJson = path.join(buildPath, "package.json")
-                const newPckgJson = path.join(buildPath, "_package.json")
+                const oldPckgJson = path.join(buildPath, 'package.json')
+                const newPckgJson = path.join(buildPath, '_package.json')
 
                 fs.renameSync(oldPckgJson, newPckgJson)
 
-                const npmInstall = spawn("npm", commands, {
+                const npmInstall = spawn('npm', commands, {
                     cwd: buildPath,
-                    stdio: "inherit",
-                    shell: true,
+                    stdio: 'inherit',
+                    shell: true
                 })
 
-                npmInstall.on("close", (code) => {
+                npmInstall.on('close', (code) => {
                     if (code === 0) {
                         fs.renameSync(newPckgJson, oldPckgJson)
 
@@ -69,43 +81,47 @@ const config: ForgeConfig = {
                          * On windows code signing fails for ARM binaries etc.,
                          * we remove them here
                          */
-                        if (platform === "win32") {
+                        if (platform === 'win32') {
                             const problematicPaths = [
-                                "android-arm",
-                                "android-arm64",
-                                "darwin-x64+arm64",
-                                "linux-arm",
-                                "linux-arm64",
-                                "linux-x64",
+                                'android-arm',
+                                'android-arm64',
+                                'darwin-x64+arm64',
+                                'linux-arm',
+                                'linux-arm64',
+                                'linux-x64'
                             ]
 
                             problematicPaths.forEach((binaryFolder) => {
                                 fs.rmSync(
                                     path.join(
                                         buildPath,
-                                        "node_modules",
-                                        "@serialport",
-                                        "bindings-cpp",
-                                        "prebuilds",
+                                        'node_modules',
+                                        '@serialport',
+                                        'bindings-cpp',
+                                        'prebuilds',
                                         binaryFolder
                                     ),
-                                    {recursive: true, force: true}
+                                    { recursive: true, force: true }
                                 )
                             })
                         }
 
                         resolve()
                     } else {
-                        reject(new Error("process finished with error code " + code))
+                        reject(
+                            new Error(
+                                'process finished with error code ' + code
+                            )
+                        )
                     }
                 })
 
-                npmInstall.on("error", (error) => {
+                npmInstall.on('error', (error) => {
                     reject(error)
                 })
             })
-        },
-    },
+        }
+    }
 }
 
 export default config
